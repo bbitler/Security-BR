9443e63a6ac30c987315d54ad950d31ca1d6ecc0
bbitler <bbitler22@gmail.com>
2017-03-19 17:23:25 -0400

    Add files via upload

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace AccessRights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = @"C:\FileAccessTest.txt";
+
+            using (FileStream stream = File.Open(filename, FileMode.Open))
+            {
+                FileSecurity securityAC = stream.GetAccessControl();
+                AuthorizationRuleCollection ruleSet = securityAC.GetAccessRules(true, true, typeof(NTAccount));
+
+                foreach (AuthorizationRule singleRule in ruleSet)
+                {
+                    var fr = singleRule as FileSystemAccessRule;
+                    WriteLine($"User's Identity: {fr.IdentityReference.Value}");
+                    WriteLine($"User's Access Type: {fr.AccessControlType}");
+                    WriteLine($"User's Rights: {fr.FileSystemRights}");                    
+                    WriteLine();
+                }
+            }
+        }
+    }
+}
