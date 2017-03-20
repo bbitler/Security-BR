8d7d0995dc4d2ff96ad4685f496bc5ff43d2f5ed
Matthew Robison <anxel9302@gmail.com>
2017-03-19 23:03:17 -0400

    Add files via upload

--- ConsoleApplication5/ConsoleApplication5/Program.cs
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -5,25 +5,37 @@ using System.Security.Principal;
 
 namespace AccessRights
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             string filename = @"C:\FileAccessTest.txt";
+            AuthorizationRuleCollection ruleset = CreateRuleSet(filename);
+            PrintRules(ruleset);
 
+         
+        }
+
+        public static AuthorizationRuleCollection CreateRuleSet(string filename)
+        {
             using (FileStream stream = File.Open(filename, FileMode.Open))
             {
                 FileSecurity securityAC = stream.GetAccessControl();
                 AuthorizationRuleCollection ruleSet = securityAC.GetAccessRules(true, true, typeof(NTAccount));
+                return ruleSet;
 
-                foreach (AuthorizationRule singleRule in ruleSet)
-                {
-                    var fr = singleRule as FileSystemAccessRule;
-                    WriteLine($"User's Identity: {fr.IdentityReference.Value}");
-                    WriteLine($"User's Access Type: {fr.AccessControlType}");
-                    WriteLine($"User's Rights: {fr.FileSystemRights}");                    
-                    WriteLine();
-                }
+
+            }
+        }
+        public static void PrintRules(AuthorizationRuleCollection ruleSet)
+        {
+            foreach (AuthorizationRule singleRule in ruleSet)
+            {
+                var fr = singleRule as FileSystemAccessRule;
+                WriteLine($"User's Identity: {fr.IdentityReference.Value}");
+                WriteLine($"User's Access Type: {fr.AccessControlType}");
+                WriteLine($"User's Rights: {fr.FileSystemRights}");
+                WriteLine();
             }
         }
     }
---
+++ ConsoleApplication5/ConsoleApplication5Tests/ProgramTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AccessRights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+
+namespace AccessRights.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CreateRuleSetTest()
+        {
+            
+            string filename = @"C:\FileAccessTest.txt";
+            AuthorizationRuleCollection ruleset = Program.CreateRuleSet(filename);
+            Assert.IsInstanceOfType(ruleset, typeof(AuthorizationRuleCollection));
+        }
+
+        [TestMethod()]
+        public void PrintRulesTest()
+        {
+            string filename = @"C:\FileAccessTest.txt";
+            AuthorizationRuleCollection ruleset = Program.CreateRuleSet(filename);
+            Program.PrintRules(ruleset);
+            Assert.Fail();
+        }
+    }
+}
